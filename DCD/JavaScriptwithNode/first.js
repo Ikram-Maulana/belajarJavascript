50efb0c55cfb8a131833fb7e2ba38a6e9b4735c6
Ikram-Maulana <ikram075@ummi.ac.id>
2021-09-03 06:58:24 +0700

    Komentar

--- DCD/JavaScriptwithNode/first.js
+++ DCD/JavaScriptwithNode/first.js
@@ -8,3 +8,11 @@ const lala3;
 // Expression
 5;
 3 + 7;
+
+// Ini merupakan komentar satu baris, kode di bawah tidak akan dijalankan
+// console.log("Halo!");
+
+/* Ini merupakan komentar dengan lebih dari satu baris
+Teks apapun yang berada disini akan dijadikan komentar.
+Ketika menggunakan ini, jangan lupa untuk menutupnya.
+*/
