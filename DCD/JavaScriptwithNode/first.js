2b6eb521b4419fc7faf7dd64e958bb5b539600e0
Ikram-Maulana <ikram075@ummi.ac.id>
2021-09-03 07:21:20 +0700

    Variabel

--- DCD/JavaScriptwithNode/first.js
+++ DCD/JavaScriptwithNode/first.js
@@ -1,3 +1,4 @@
+// ==================================================================================================
 console.log("Hello, World!");
 
 // Statement
@@ -9,6 +10,8 @@ const lala3;
 5;
 3 + 7;
 
+// ==================================================================================================
+
 // Ini merupakan komentar satu baris, kode di bawah tidak akan dijalankan
 // console.log("Halo!");
 
@@ -16,3 +19,19 @@ const lala3;
 Teks apapun yang berada disini akan dijadikan komentar.
 Ketika menggunakan ini, jangan lupa untuk menutupnya.
 */
+
+// ==================================================================================================
+
+let fullName =
+  let lastName; // Error karena let lastName adalah sebuah statement untuk deklarasi variabel. Statement tidak bisa berada di posisi expression.
+let fullName = (lastName = "Skywalker"); // (lastName = "Skywalker") merupakan expression, sehingga kode ini tidak error.
+let fullName = "Luke" + "Skywalker"; // "Luke" + "Skywalker" juga merupakan expression, sehingga kode ini tidak error.
+
+const z = 100;
+console.log(z);
+z = 200;
+console.log(z)
+
+/* TypeError: Assignment to constant variable. */
+
+// ==================================================================================================
