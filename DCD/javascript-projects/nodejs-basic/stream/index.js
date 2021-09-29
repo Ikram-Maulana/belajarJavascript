2c516e9f656ff6824aaf4d7d58811ce83596cf61
ikram-maulana <ikram075@ummi.ac.id>
2021-09-29 19:25:02 +0700

    Add Stream Projects

---
+++ DCD/javascript-projects/nodejs-basic/stream/index.js
@@ -0,0 +1,27 @@
+/**
+ * TODO:
+ * Buatlah program untuk membaca teks input.txt dan menuliskannya ulang pada berkas output.txt
+ * menggunakan teknik readable stream dan writable stream.
+ */
+
+const fs = require("fs");
+const {
+  resolve
+} = require("path");
+
+const readableStream = fs.createReadStream(resolve(__dirname, "input.txt"), {
+  highWaterMark: 15
+});
+const writableSystem = fs.createWriteStream(resolve(__dirname, "output.txt"));
+
+readableStream.on("readable", () => {
+  try {
+    writableSystem.write(`${readableStream.read()}\n`);
+  } catch (error) {
+
+  }
+});
+
+readableStream.on("end", () => {
+  console.log("Done");
+});
