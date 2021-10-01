be7d9b31b4c1e0c332b03f5d8d330b83e4fbddd7
ikram-maulana <ikram075@ummi.ac.id>
2021-10-01 08:17:27 +0700

    Menambahkan Logic Menyimpan Catatan

---
+++ DCD/javascript-projects/notes-app-back-end/src/routes.js
@@ -0,0 +1,13 @@
+const {
+  addNotesHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNotesHandler,
+  },
+];
+
+module.exports = routes;
