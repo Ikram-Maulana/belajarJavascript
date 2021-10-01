654a73495aff784f794246f92b0a4abe1f0d98d6
ikram-maulana <ikram075@ummi.ac.id>
2021-10-01 18:27:26 +0700

    Menambahkan Logic Delete Notes

--- DCD/javascript-projects/notes-app-back-end/src/handler.js
+++ DCD/javascript-projects/notes-app-back-end/src/handler.js
@@ -6,7 +6,7 @@ const addNoteHandler = (request, h) => {
 
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
-  const updateAt = createdAt;
+  const updatedAt = createdAt;
 
   const newNotes = {
     title,
@@ -14,7 +14,7 @@ const addNoteHandler = (request, h) => {
     body,
     id,
     createdAt,
-    updateAt,
+    updatedAt,
   };
 
   notes.push(newNotes);
@@ -68,7 +68,7 @@ const getNotesById = (request, h) => {
 const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
   const { title, tags, body } = request.payload;
-  const createdAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
 
   const index = notes.findIndex((note) => note.id === id);
 
@@ -78,7 +78,7 @@ const editNoteByIdHandler = (request, h) => {
       title,
       tags,
       body,
-      createdAt,
+      updatedAt,
     };
 
     const response = h.response({
@@ -96,6 +96,28 @@ const editNoteByIdHandler = (request, h) => {
   return response;
 };
 
+const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
-  addNoteHandler, getAllNotesHandler, getNotesById, editNoteByIdHandler,
+  addNoteHandler, getAllNotesHandler, getNotesById, editNoteByIdHandler, deleteNoteByIdHandler,
 };
